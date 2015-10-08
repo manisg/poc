6e0e2c6e573514e76ddf68684931875b0152ee5a
computer-pc <computer-pc@yuvan>
2015-10-09 01:49:27 +0530

    first change

--- myproject/src/mypackage/Student.java
+++ myproject/src/mypackage/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public void displaydata() {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to sgsgc");
+		System.out.println("first change in git ");
 	}
 
 }
