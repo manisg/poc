8413e30d709143d1c627577d24a0ac7b41dfd6aa
computer-pc <computer-pc@yuvan>
2015-10-09 02:53:48 +0530

    array added

--- myproject/src/mypackage/Student.java
+++ myproject/src/mypackage/Student.java
@@ -6,7 +6,10 @@ public class Student {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to sgsgc");
 		System.out.println("first change in git ");
-		
+		String[] a={"a","b","c"};
+		for (String string : a) {
+		System.out.println(string);	
+		}
 	}
 
 }
